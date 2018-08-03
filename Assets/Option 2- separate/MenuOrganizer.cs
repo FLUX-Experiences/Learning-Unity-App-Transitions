dec02181b4db5882c8fc5a4df361ff7162834395
Shachar Oz <shachar.oz@gmail.com>
2018-08-04 01:57:49 +0300

    menu system screen transitions example

---
+++ Assets/Option 2- separate/MenuOrganizer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOrganizer : MonoBehaviour {
+
+    private List<MenuScreen> Screens;
+    private MenuScreen _currentScreen;
+
+    private void Awake()
+    {
+        Screens = new List<MenuScreen>();
+        for (int i=0; i < transform.childCount; i++)
+        {
+            Screens.Add(transform.GetChild(i).GetComponent<MenuScreen>());
+        }
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SwitchScreen(Screens[0]);
+
+    }
+    
+    public void SwitchScreen(MenuScreen newScreen)
+    {
+        if (newScreen != _currentScreen)
+        {
+            newScreen.Transition_IN();
+            if (_currentScreen != null) _currentScreen.Transition_OUT();
+
+            _currentScreen = newScreen;
+        }
+    }
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Option 2- separate/MenuScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreen : MonoBehaviour {
+
+    public string Title;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public string Transition_In_Trigger;
+    public string Transition_Out_Trigger;
+    public void Transition_IN()
+    {
+        _animator.SetTrigger(Transition_In_Trigger);
+    }
+    public void Transition_OUT()
+    {
+        _animator.SetTrigger(Transition_Out_Trigger);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
